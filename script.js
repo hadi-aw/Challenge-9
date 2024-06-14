c53668fc408770d48c1d72012bd42ac0cc882a50
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-06-14 11:22:38 -0400

    script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 function main() {
     d3.select('body').append('div')
+    
     d3.selectAll('div')
     .on('mouseover', function() {
         d3.select(this).style('background-color', 'firebrick');
     })
     .on('mouseout', function() {
-        d3.select(this).style('background-color', 'steelblue');
+        d3.select(this).style('background-color', 'blue');
 });
 }
 
